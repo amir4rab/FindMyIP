689b4b047bf52f67613b2e7ea6b3fa3645ae0d25
amir4rab <60965542+amir4rab@users.noreply.github.com>
2020-08-27 19:07:07 +0430

    1

--- src/js/api/UnSplash.js
+++ src/js/api/UnSplash.js
@@ -6,7 +6,7 @@ export class UnSplash{
         let img;
         document.getElementById("ipAddressStatus").innerText = `https://source.unsplash.com/${window.innerHeight/2}x${window.innerWidth}/?city,${data}`;
         if(window.innerWidth < 800){
-            img = await fetch(`https://source.unsplash.com/${window.innerHeight/2}x${window.innerWidth}/?city,${data}`);
+            img = await fetch(`https://source.unsplash.com/${window.innerHeight<720?720:window.innerHeight}x${window.innerWidth<720?720:window.innerWidth}/?city,${data}`);
         }else{
             img = await fetch(`https://source.unsplash.com/${window.innerHeight>1080?1080:window.innerHeight}x${window.innerWidth>1920?1920:window.innerWidth}/?city,${data}`);
         }
